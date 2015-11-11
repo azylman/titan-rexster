bc04801197fe624caffaf0f5b5a46c06b2e663fa
Alex Zylman <alex.zylman@gmail.com>
2015-11-11 15:33:59 -0800

    allow configurable hosts

--- run.sh
+++ run.sh
@@ -11,5 +11,6 @@ cp $IN $OUT
 # Use hash instead of slash because the hostport has a slash
 sed -i "s#_DYNAMODB_HOSTPORT_#${DYNAMODB_HOSTPORT}#g" $OUT
 sed -i "s#_GRAPH_NAME_#${GRAPH_NAME}#g" $OUT
+sed -i "s#_BASE_URI_#${BASE_URI}#g" $OUT
 
 $BIN/rexster.sh -s -c ../$OUT
